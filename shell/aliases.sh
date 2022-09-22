74c7ac05f7fa3435f185c5aa26b9fe13e760eede
gavilanbe <nahuel@gavilanbe.io>
2022-09-22 17:16:28 +0200

    themes config added

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,24 +3,21 @@ alias sudo='sudo '
 
 alias ..="cd .."
 alias ...="cd ../.."
+alias ls="exa"
 alias ll="exa -l --icons"
 alias la="exa -la --icons"
 
 # Jumps
 alias ~="cd ~"
 alias tmp="cd ~/Desktop/tmp"
-alias c='cd ~/Code'
-alias codely="cd ~/Code/codely"
 
 # Git
 alias gaa="git add -A"
-alias gc="dot git commit"
 alias gca="git add --all && git commit --amend --no-edit"
 alias gco="git checkout"
-alias gd="dot git pretty-diff"
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gps="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
-alias gl="dot git pretty-log"
+alias gl="git pretty-log"
--- shell/zsh/.zshrc
+++ shell/zsh/.zshrc
@@ -5,7 +5,8 @@ source "$DOTFILES_PATH/shell/init.sh"
 # Start zim
 source "$ZIM_HOME/init.zsh"
 
-#source "$DOTFILES_PATH/shell/zsh/themes/spaceship.zsh"
+#PROMPT 
+source "$DOTFILES_PATH/shell/zsh/themes/spaceship.zsh"
 
 # Bindings
 
